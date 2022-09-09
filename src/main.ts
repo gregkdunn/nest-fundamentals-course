a817d3d42d6c2a764afc0b751c750e2aa5a91141
Greg Dunn <greg.dunn@callrail.com>
2022-09-09 08:20:26 -0400

    Custom Exception Filter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Globally Bound Pipe - useGlobalPipe
   //app.use
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
