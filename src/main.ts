5ed3ccbb7628a17f5f2edff5d9421b04ae8af668
Greg Dunn <greg.dunn@callrail.com>
2022-09-09 08:54:50 -0400

    Custom Guard

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ApiKeyGuard } from './common/guards/api-key.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Globally Bound Pipe - useGlobalPipe
   //app.use
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalGuards(new ApiKeyGuard());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
